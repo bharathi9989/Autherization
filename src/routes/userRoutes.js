207347981fedc219ce9df5dca3e70b7f4da91518
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-07 09:10:50 +0530

    update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../middlewares/authMiddleware.js";
 const userRoutes = Router();
 
 userRoutes.post("/register", register);
-userRoutes.get("/login", login);
+userRoutes.post("/login", login);
 userRoutes.get("/login/:_id", verifyToken, getMe);
 
 export default userRoutes;
